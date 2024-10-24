020e9c09b81c8c5e156ef269c55c6f672bffeddf
Murad Hossain <muradhossen94848@gmail.com>
2024-10-24 15:02:15 +0600

    updated

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 var slider = tns({
-  container: '.feature_item',
-  items: 3,
-  nav: false,
-  mouseDrag: true,
-  responsive: {
+    container: '.feature_item',
+    items: 3,
+    nav: false,
+    mouseDrag: true,
+    responsive: {
+        "1200": {
+          "items": 3,
+        },
+        "900": {
+            "items": 2,
+        },
+        "640": {
+            "items": 1,
+        },
+        "400": {
+            "items": 1,
+        },
+        "340": {
+            "items": 1,
+        },
+    },
+    controls: true,
+    controlsPosition: "bottom",
+    controlsText:  ["prev", "next"],
+    controlsContainer: "#control",
+    prevButton: ".prev",
+    nextButton: ".next",
+  });
+  
+  var slider = tns({
+    container: '.post_entries',
+    items: 3,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    nav: false,
+    mouseDrag: true,
+    responsive: {
       "1200": {
-        "items": 3,
+          "items": 3,
       },
       "900": {
           "items": 2,
@@ -16,94 +48,74 @@ var slider = tns({
       "400": {
           "items": 1,
       },
+      "340": {
+          "items": 1,
+      },
   },
-  controls: true,
-  controlsPosition: "bottom",
-  controlsText:  ["prev", "next"],
-  controlsContainer: "#controls",
-  prevButton: ".prev",
-  nextButton: ".next",
-});
-
-var slider = tns({
-  container: '.post_entries',
-  items: 3,
-  autoplay: true,
-  autoplayButtonOutput: false,
-  nav: false,
-  mouseDrag: true,
-  responsive: {
-    "1200": {
-        "items": 3,
+    controls: true,
+    controlsPosition: "bottom",
+    controlsText:  ["prev", "next"],
+    controlsContainer: "#controls",
+    prevButton: ".prevs",
+    nextButton: ".nexts",
+  });
+  
+  var slider = tns({
+    container: '.testimonial-slider',
+    items: 3,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    nav: false,
+    mouseDrag: true,
+    responsive: {
+      "1200": {
+          "items": 3,
+      },
+      "900": {
+          "items": 2,
+      },
+      "640": {
+          "items": 1,
+      },
+      "400": {
+          "items": 1,
+      },
+      "340": {
+          "items": 1,
+      },
     },
-    "900": {
-        "items": 2,
+    controls: true,
+    controlsPosition: "bottom",
+    controlsText:  ["prev", "next"],
+    controlsContainer: "#controlss",
+    prevButton: ".prevss",
+    nextButton: ".nextss",
+  });
+  
+  
+  var slider = tns({
+    container: '.my-slider',
+    items: 4,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    nav: false,
+    mouseDrag: true,
+    controls: false,
+    responsive: {
+      "1200": {
+          "items": 4,
+      },
+      "900": {
+          "items": 3,
+      },
+      "640": {
+          "items": 2,
+      },
+      "400": {
+          "items": 1,
+      },
+      "340": {
+          "items": 1,
+      },
     },
-    "640": {
-        "items": 1,
-    },
-    "400": {
-        "items": 1,
-    },
-},
-  controls: true,
-  controlsPosition: "bottom",
-  controlsText:  ["prev", "next"],
-  controlsContainer: "#controls",
-  prevButton: ".prev",
-  nextButton: ".next",
-});
-
-var slider = tns({
-  container: '.testimonial-slider',
-  items: 3,
-  autoplay: true,
-  autoplayButtonOutput: false,
-  nav: false,
-  mouseDrag: true,
-  responsive: {
-    "1200": {
-        "items": 3,
-    },
-    "900": {
-        "items": 2,
-    },
-    "640": {
-        "items": 1,
-    },
-    "400": {
-        "items": 1,
-    },
-  },
-  controls: true,
-  controlsPosition: "bottom",
-  controlsText:  ["prev", "next"],
-  controlsContainer: "#controls",
-  prevButton: ".prev",
-  nextButton: ".next",
-});
-
-
-var slider = tns({
-  container: '.my-slider',
-  items: 4,
-  autoplay: true,
-  autoplayButtonOutput: false,
-  nav: false,
-  mouseDrag: true,
-  controls: false,
-  responsive: {
-    "1200": {
-        "items": 4,
-    },
-    "900": {
-        "items": 3,
-    },
-    "640": {
-        "items": 2,
-    },
-    "400": {
-        "items": 1,
-    },
-  },
-});
+  });
